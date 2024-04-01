d6e8cbdc3f9eb7dddda286e27142fd10f7d92a2f
andrewtroicki <atroicki@icloud.com>
2024-04-01 15:41:39 +1100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) 
+  return 'This one is on me!'
+  }
   // Write your code here!
-}
+
 
 function ternaryCheckCity(){
   // Write your code here!
