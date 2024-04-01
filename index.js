cdb7db26a3e5b384d9ef73673589527f6dd20fe9
andrewtroicki <atroicki@icloud.com>
2024-04-01 16:33:18 +1100

    4 tests passed

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 function scuberGreetingForFeet(distance){
-  if (distance <= 400) 
-  return 'This one is on me!'
-  }
-  // Write your code here!
+  if (distance <= 400) {
+  return 'This one is on me!' 
+
+} else if (distance >= 400 && distance <= 2000) {
+  return 'That will be twenty bucks.';
+
+} else if (distance >= 2001 && distance <=2499) {
+  return 'I will gladly take your thirty bucks.';
+  
+} else if (distance > 2500) {
+  return 'No can do.'
+}
+
+}
 
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+  
+
+
+
+function ternaryCheckCity(distance){
+  let distanceOne = 400;
+  let distanceTwo = 2000;
+
+   
+  
+  
 }
 
 function switchOnCharmFromTip(){
