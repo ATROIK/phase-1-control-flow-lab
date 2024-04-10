a1739bb51406b2f87204eefcda0ef49a69025f73
andrewtroicki <atroicki@icloud.com>
2024-04-10 13:43:23 +1000

    tests passed

--- index.js
+++ index.js
@@ -7,29 +7,33 @@ function scuberGreetingForFeet(distance){
 
 } else if (distance >= 2001 && distance <=2499) {
   return 'I will gladly take your thirty bucks.';
-  
+
 } else if (distance > 2500) {
   return 'No can do.'
 }
 
 }
 
-
-
-
-  
-
-
-
-function ternaryCheckCity(distance){
-  let distanceOne = 400;
-  let distanceTwo = 2000;
-
-   
-  
-  
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? 'Ok, sounds good.' :  'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+
+function switchOnCharmFromTip(tip){
+  let tip1 = 'generous';
+  let tip2 = 'not as generous';
+
+  switch(tip) {
+    case tip1: 'generous'
+      return 'Thank you so much.';
+    case tip2: 'not as generous'  
+      return 'Thank you.';
+    default:
+      return 'Bye.'  
+  }
 }
+
+
